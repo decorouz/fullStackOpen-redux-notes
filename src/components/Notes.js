7501d94cb85676af808fd988532c316ee48578f6
Biola <abiola2326@gmail.com>
2020-05-27 17:04:47 +0100

    part6-2

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -6,14 +6,23 @@ const Note = ({ note, handleClick }) => {
   return (
     <li onClick={handleClick}>
       {note.content}
-      <strong>{note.important ? 'important' : ''}</strong>
+      <strong>{note.important ? ' important' : ''}</strong>
     </li>
   )
 }
 
 const Notes = () => {
   const dispatch = useDispatch()
-  const notes = useSelector((state) => state)
+
+  const notes = useSelector(({ filter, notes }) => {
+    console.log(notes)
+    if (filter === 'ALL') {
+      return notes
+    }
+    return filter === 'IMPORTANT'
+      ? notes.filter((note) => note.important)
+      : notes.filter((note) => !note.important)
+  })
 
   return (
     <ul>

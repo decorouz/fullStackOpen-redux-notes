bb918713627c1231a9c1f5daed7bac6e3b3d007b
Biola <abiola2326@gmail.com>
2020-06-11 16:17:25 +0100

    part6-5 connect implementation

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { toggleImportanceOf } from '../reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 
 const Note = ({ note, handleClick }) => {
   return (
@@ -12,28 +12,35 @@ const Note = ({ note, handleClick }) => {
 }
 
 const Notes = (props) => {
-  const dispatch = useDispatch()
-
-  const notes = useSelector(({ filter, notes }) => {
-    if (filter === 'ALL') {
-      return notes
-    }
-    return filter === 'IMPORTANT'
-      ? notes.filter((note) => note.important)
-      : notes.filter((note) => !note.important)
-  })
-
   return (
     <ul>
-      {notes.map((note) => (
+      {props.notes.map((note) => (
         <Note
           key={note.id}
           note={note}
-          handleClick={() => dispatch(toggleImportanceOf(note))}
+          handleClick={() => props.toggleImportanceOf(note)}
         />
       ))}
     </ul>
   )
 }
 
-export default Notes
+const mapStateToProps = (state) => {
+  if (state.filter === 'ALL') {
+    return { notes: state.notes }
+  }
+  return {
+    notes:
+      state.filter === 'IMPORTANT'
+        ? state.notes.filter((note) => note.important)
+        : state.notes.filter((note) => !note.important),
+  }
+}
+
+const mapDispatchToProps = {
+  toggleImportanceOf,
+}
+
+const ConnectedNotes = connect(mapStateToProps, mapDispatchToProps)(Notes)
+
+export default ConnectedNotes
